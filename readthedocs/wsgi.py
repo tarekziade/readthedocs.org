4f9642dcda89448e424d93dedcd0a919c8de49eb
Tarek Ziade <tarek@ziade.org>
2012-07-20 17:34:15 +0200

    added readthedocs in the path

--- readthedocs/wsgi.py
+++ readthedocs/wsgi.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'readthedocs.settings.sqlite'
